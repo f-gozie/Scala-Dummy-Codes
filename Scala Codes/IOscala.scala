ca4131dfa7940291f657e10eb75b46e037611641
Agozie Favour <fagozie43@gmail.com>
2018-10-09 01:33:26 +0100

    Changed some itty-bitty stuff

--- Scala Codes/IOscala.scala
+++ Scala Codes/IOscala.scala
@@ -12,5 +12,12 @@ var age = 18
 printf("Hello, %s! You are %d years old. \n", name, age)
 
 // String interpolation
-print(f"Hello, $name! In six months, you'll be age${age + 0.5}%7.2f years old")
+print(f"Hello, $name! In six months, you'll be age${age + 0.5}%7.2f years old\n")
 
+// Must import io package to readline function
+// Note that import statement should conventionally be at the top of the file
+import scala.io._
+val data = StdIn.readLine("Your name: ")
+print("Your age: ")
+val num = StdIn.readInt()
+println(s"Hello, ${data}! Next year, you will be ${num + 1}.")
